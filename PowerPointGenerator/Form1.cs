5a4487d5d92094a649682d338699d5e42dd6a4c5
Erik Hanson <ErikHanson135@gmail.com>
2021-05-20 18:44:43 -0700

    Just checking in

---
+++ PowerPointGenerator/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure.CognitiveServices.Search.ImageSearch;
+using Microsoft.Azure.CognitiveServices.Search.ImageSearch.Models;
+
+namespace PowerPointGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+/*            foreach(char c in richTextBox1)
+            {
+
+            }
+            string[] splits = richTextBox1.Text.Split(new char[] { ' ', ',' });
+            Console.WriteLine(textBax1.Text);
+            Console.WriteLine(richTextBox1.Text);*/
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
